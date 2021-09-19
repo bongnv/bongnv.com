acf4b7e02e55f1a9f7a336202c10116d00f35c98
Bong Nguyen <vanbong@gmail.com>
2021-09-19 13:37:43 +1000

    feat: simpllify UI for mobile screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,76 +1,31 @@
 import React, { FC, useRef, useState, useEffect } from "react";
-import { Menu, X } from "react-feather";
 
-import MobileNav from "./mobile-nav";
 import ThemeSwitcher from "./theme-switcher";
 import Nav from "./nav";
 
 /** @jsx jsx */
-import { jsx, Box, Flex, IconButton } from "theme-ui";
+import { jsx, Box, Flex } from "theme-ui";
 
-const Header: FC = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-  const onMenuClick = () => setMenuVisible(!menuVisible);
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (menuVisible) {
-      const eventHandler = (e: Event): void => {
-        if (!ref || !ref.current || !ref.current.contains(e.target)) {
-          setMenuVisible(false);
-        }
-      };
-
-      window.addEventListener("click", eventHandler);
-      window.addEventListener("scroll", eventHandler);
-
-      return (): void => {
-        window.removeEventListener("click", eventHandler);
-        window.removeEventListener("scroll", eventHandler);
-      };
-    }
-  }, [menuVisible]);
-
-  return (
-    <Box
-      as="header"
+const Header: FC = () => (
+  <Box
+    as="header"
+    sx={{
+      marginBottom: 3,
+      paddingBottom: 2,
+      borderBottom: "1px solid",
+      borderColor: "muted",
+    }}
+  >
+    <Flex
       sx={{
-        marginBottom: 3,
-        paddingBottom: 2,
-        borderBottom: "1px solid",
-        borderColor: "muted",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
-      ref={ref}
     >
-      <Flex
-        sx={{
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <IconButton
-          title="Menu"
-          aria-label="Toggle Menu"
-          sx={{
-            marginRight: 3,
-            variant: "buttons.navIcon",
-            display: ["block", "none"],
-          }}
-          onClick={onMenuClick}
-        >
-          {menuVisible ? <X /> : <Menu />}
-        </IconButton>
-        <Nav />
-        <Box
-          sx={{
-            flexGrow: 1,
-          }}
-        />
-        <ThemeSwitcher />
-      </Flex>
-      {menuVisible && <MobileNav />}
-    </Box>
-  );
-};
+      <Nav />
+      <ThemeSwitcher />
+    </Flex>
+  </Box>
+);
 
 export default Header;
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,43 +4,36 @@ import { Link } from "gatsby";
 /** @jsx jsx */
 import { jsx, Box } from "theme-ui";
 
-const Nav: FC = () => {
-  return (
-    <Box
-      as="nav"
+const Nav: FC = () => (
+  <Box as="nav">
+    <Link
       sx={{
-        display: ["none", "block"],
+        paddingRight: [2, 3],
+        variant: "textStyles.navLink",
+      }}
+      to="/"
+    >
+      <span>Home</span>
+    </Link>
+    <Link
+      to="/blog/"
+      sx={{
+        padding: [2, 3],
+        variant: "textStyles.navLink",
       }}
     >
-      <Link
-        sx={{
-          paddingRight: 3,
-          variant: "textStyles.navLink",
-        }}
-        to="/"
-      >
-        <span>Home</span>
-      </Link>
-      <Link
-        to="/blog/"
-        sx={{
-          padding: 3,
-          variant: "textStyles.navLink",
-        }}
-      >
-        <span>Blog</span>
-      </Link>
-      <Link
-        to="/about/"
-        sx={{
-          padding: 3,
-          variant: "textStyles.navLink",
-        }}
-      >
-        <span>About</span>
-      </Link>
-    </Box>
-  );
-};
+      <span>Blog</span>
+    </Link>
+    <Link
+      to="/about/"
+      sx={{
+        padding: [2, 3],
+        variant: "textStyles.navLink",
+      }}
+    >
+      <span>About</span>
+    </Link>
+  </Box>
+);
 
 export default Nav;
