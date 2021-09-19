a2ca96b15ea95fdb6a2b19e18edeb560553e94fa
Bong Nguyen <vanbong@gmail.com>
2021-09-19 18:16:40 +1000

    feat: update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from "react";
 
+import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { Themed } from "theme-ui";
 
 const NotFoundPage: FC = () => (
-  <>
+  <Layout>
     <SEO title="404: Not found" />
-    <main className="w-full max-w-2xl content">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </main>
-  </>
+    <Themed.h1>Not Found</Themed.h1>
+    <Themed.p>
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </Themed.p>
+  </Layout>
 );
 
 export default NotFoundPage;
